901cb5e0870a5227d560848798c00e3d0830f9bc
carlos augusto <carlos.xell.3@gmail.com>
2022-02-21 22:28:02 -0300

    remoção de classes de estados que não seriam usadas, refatoração final de header component, adição de ícone para botão voltar do header

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,6 +1,6 @@
 <script>
-	import { page } from '$app/stores';
-	import { onMount, afterUpdate } from 'svelte';
+	import {page} from '$app/stores';
+	import {afterUpdate, onMount} from 'svelte';
 
 	onMount(() => {
 		console.info($page, 'Dados da página no carregamento');
@@ -13,12 +13,54 @@
 
 <header class="header">
 	<div class="header__content">
-		<a sveltekit:prefetch href="/" class="header__link" title="Voltar">
-			<img src="./static/icons/ic--arrow-left.svg" alt="" />
-		</a>
-		<h1 class="header__title">SEVN NEWS</h1>
+		{#if $page.url.pathname !== '/'}
+			<a sveltekit:prefetch href="/" class="header__link" title="Voltar">
+				<img src="./static/icons/ic--arrow-left.svg" alt="" />
+			</a>
+		{/if}
+		<h1 class="header__title">sevn news</h1>
 	</div>
 </header>
 
 <style lang="scss">
+	.header {
+		position: fixed;
+		background-color: var(--c-yellow);
+		height: 54px;
+		left: 0;
+		right: 0;
+		top: 0;
+
+		&__content {
+			display: flex;
+			height: 100%;
+			position: relative;
+			width: 100%;
+		}
+
+		&__link {
+			align-items: center;
+			background-color: var(--c-black);
+			display: flex;
+			height: 100%;
+			justify-content: center;
+			left: 0;
+			position: absolute;
+			width: 54px;
+
+			img {
+				height: 30px;
+				width: 32px;
+			}
+		}
+
+		&__title {
+			color: var(--c-black);
+			font-size: var(--gap-md);
+			font-weight: var(--fw-bold);
+			line-height: 30px;
+			margin: auto;
+			text-transform: uppercase;
+		}
+	}
 </style>
